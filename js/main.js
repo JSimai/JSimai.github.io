46205f98edf5e7961e50b2970dfbe07b26163a6d
JSimai <jsimai@student.unimelb.edu.au>
2022-05-31 14:39:29 +1000

    Product Grid, Search, and Login done.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,45 @@ function closePopup(id) {
   popup.style.transform = "translateX(200%)";
 }
 
+function toggleSearch() {
+  var search = document.getElementById("searchBar");
+  var box = document.getElementById("backBox");
+  if (search.style.display === "none") {
+    search.style.display = "flex";
+    box.style.display = "flex";
+  } else {
+    search.style.display = "none";
+    box.style.display = "none";
+  }
+}
+
 function handleForm() {
-  console.log("Form Handled :D")
+  var form = document.getElementById("loginCont");
+  var greeting = document.getElementById("greeting");
+  form.style.display = "none";
+  greeting.style.display = "flex";
+}
+
+function logout() {
+  var form = document.getElementById("loginCont");
+  var greeting = document.getElementById("greeting");
+  greeting.style.display = "none";
+  form.style.display = "block";
+}
+
+function select(id, id1) {
+  var element = document.getElementById(id);
+  var element1 = document.getElementById(id1);
+  if (element.style.display === "none") {
+  element.style.display = "flex";
+  }
+  else {
+  element.style.display = "none";
+  }
+  if (element1.style.display === "none") {
+  element1.style.display = "flex";
+  }
+  else {
+  element1.style.display = "none";
+  }
 }
